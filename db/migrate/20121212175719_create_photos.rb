25b6866908b45bebc8c6aec343c11f9a525a0bd0
Vsevolod Rodionov <crbrus@gmail.com>
2012-12-14 00:43:00 +0400

    init

--- db/migrate/20121212175719_create_photos.rb
+++ db/migrate/20121212175719_create_photos.rb
@@ -2,7 +2,6 @@ class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
       t.string :link
-      t.string :author
       t.boolean :is_legal
 
       t.timestamps
