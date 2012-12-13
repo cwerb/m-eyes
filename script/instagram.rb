6a43960724d9b1df408cc9b19ccc00fb9cb6b830
Vsevolod Rodionov <crbrus@gmail.com>
2012-12-13 14:45:26 +0400

    init

--- script/instagram.rb
+++ script/instagram.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
 require 'daemons'
-Daemons.run_proc('instagram.rb') do
+Daemons.run_proc('instagram.rb', multiple: false, ontop: true) do
 
 require 'active_record'
 ActiveRecord::Base.establish_connection YAML::load(File.open 'config/database.yml')[ENV["RAILS_ENV"] || 'development']
