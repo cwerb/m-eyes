11a5f8041c93789cbab4c17cd275b7b24596ea44
Vsevolod Rodionov <crbrus@gmail.com>
2012-12-13 20:46:53 +0400

    init

--- db/migrate/20121213153833_create_authors.rb
+++ db/migrate/20121213153833_create_authors.rb
@@ -1,7 +1,7 @@
 class CreateAuthors < ActiveRecord::Migration
   def change
     create_table :authors do |t|
-      t.integer :nickname
+      t.string :nickname
       t.boolean :is_banned
 
       t.timestamps
