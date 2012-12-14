079d8b102d5cd2948ae33066c5006b4534739a34
Vsevolod Rodionov <crbrus@gmail.com>
2012-12-14 19:02:18 +0400

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,15 @@
 MEyes::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.sendmail_settings = {
+      :location => '/usr/sbin/sendmail',
+      :arguments => '-i -t'
+  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
