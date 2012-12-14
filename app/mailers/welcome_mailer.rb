111a16a29424062ed0d21e34237acf5848bc3b16
Kir <kir@tinve.ru>
2012-12-14 21:38:12 +0400

    Тема письма изменилась

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -3,7 +3,7 @@ class WelcomeMailer < ActionMailer::Base
   default from: "russia.maybelline@gmail.com"
   def welcome(user)
     @user = user
-    mail(:to => user.email, :subject => "Мейбилин любит тебя") do |format|
+    mail(:to => user.email, :subject => "Подтверждение участия в конкурсе") do |format|
       format.html
     end
   end
