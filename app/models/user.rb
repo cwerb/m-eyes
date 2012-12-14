f14452ff846f22a2fa140fe84366a1184ed49c05
Vsevolod Rodionov <crbrus@gmail.com>
2012-12-14 18:58:48 +0400

    init

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # -*- encoding : utf-8 -*-
-require 'digest/md5'
 class User < ActiveRecord::Base
+  require 'digest/md5'
   attr_accessible :email, :username, :register_token
   validates :email, email: true
   validates :username, presence: true, uniqueness: true
